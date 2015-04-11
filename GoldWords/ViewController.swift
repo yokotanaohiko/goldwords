4ae8b66d68f13a88dd05c8037507896df10fb0a7
iam4570 <iam4570@yahoo.co.jp>
2015-04-12 01:28:22 +0900

    Initial Commit

---
+++ GoldWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoldWords
+//
+//  Created by yokota on 2015/04/12.
+//  Copyright (c) 2015年 yokota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
